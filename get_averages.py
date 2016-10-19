f827bab2aa200fd2a9cbcbc079e8ba80f7271929
Issac Kim <ikim1@stuy.edu>
2016-10-18 22:48:21 -0400

    write get_averages

--- get_averages.py
+++ get_averages.py
@@ -9,7 +9,16 @@ cmd = "SELECT name, students.id, mark FROM students, courses WHERE students.id =
 info = c.execute(cmd)
 students = {}
 for item in info:
-    
-    student[item[0]] = [item[1], ]
+    students[item[0]] = [item[1], 0, 0]
+info = c.execute(cmd)
+for item in info:
+    grade = students[item[0]][1] + item[2]
+    num_courses = students[item[0]][2] + 1
+    students[item[0]][1] = grade
+    students[item[0]][2] = num_courses
+s = "NAME, ID, AVAERAGE\n"
+for key in students:
+    s += "%s, %d, %f\n" %(key, students[key][0], students[key][1]/students[key][2])
+print(s)
     
 
