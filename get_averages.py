a08dfb66389be087e750142f90455d324e71354d
Issac Kim <ikim1@stuy.edu>
2016-10-18 22:50:50 -0400

    fix floating point issue

--- get_averages.py
+++ get_averages.py
@@ -9,11 +9,11 @@ cmd = "SELECT name, students.id, mark FROM students, courses WHERE students.id =
 info = c.execute(cmd)
 students = {}
 for item in info:
-    students[item[0]] = [item[1], 0, 0]
+    students[item[0]] = [item[1], 0.0, 0.0]
 info = c.execute(cmd)
 for item in info:
     grade = students[item[0]][1] + item[2]
-    num_courses = students[item[0]][2] + 1
+    num_courses = students[item[0]][2] + 1.0
     students[item[0]][1] = grade
     students[item[0]][2] = num_courses
 s = "NAME, ID, AVAERAGE\n"
